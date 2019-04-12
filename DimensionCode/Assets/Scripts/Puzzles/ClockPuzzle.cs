d72a7bc8cd78d406d6bb900b7d9929ed52f4d5ed
Stefano <c.stefano6@gmail.com>
2019-04-12 17:10:07 +0200

    Added new scene for puzzle prototyping

---
+++ DimensionCode/Assets/Scripts/Puzzles/ClockPuzzle.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockPuzzle : MonoBehaviour
+{
+    public GameObject hourHand;
+    public GameObject minuteHand;
+    public float rotationSpeedHour;
+    public float rotationSpeedMinute;
+    private bool rotateHour = true;
+    private bool rotateMinute = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateHands();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            ShootRay();
+        }
+
+        if (!rotateHour && !rotateMinute)
+        {
+
+            CheckTime();
+        }
+    }
+
+    private void RotateHands()
+    {
+        if (rotateHour)
+        {
+            hourHand.transform.Rotate(0, rotationSpeedHour * Time.deltaTime, 0);
+        }
+
+        if (rotateMinute)
+        {
+            minuteHand.transform.Rotate(0, rotationSpeedMinute * Time.deltaTime, 0);
+        }
+    }
+
+    private int convertToHour(float angle)
+    {
+        float result = angle / 30f;
+
+        return (int)result;
+    }
+
+    private int convertToMinute(float angle)
+    {
+        float result = angle / 6f;
+
+        return (int)result;
+    }
+
+    void ShootRay()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Transform currentClockHand = hit.transform;
+
+            if (currentClockHand == hourHand.transform)
+            {
+                rotateHour = false;
+            }
+
+            if (currentClockHand == minuteHand.transform)
+            {
+                rotateMinute = false;
+            }
+        }
+    }
+
+    private void CheckTime()
+    {
+        Debug.Log(DateTime.Now.Hour);
+        Debug.Log(DateTime.Now.Minute);
+        int currentHour = DateTime.Now.Hour;
+
+        if (currentHour > 12)
+        {
+            currentHour = currentHour - 12;
+        }
+
+        if(convertToHour(hourHand.transform.eulerAngles.y) != currentHour)
+        {
+            rotateHour = true;
+        }
+    }
+}
---
+++ DimensionCode/Assets/Scripts/Puzzles/LockPuzzle.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockPuzzle : MonoBehaviour
+{
+    public List<GameObject> lockList;
+    public List<float> lockRotationList;
+    private List<GameObject> lockListCopy;
+    private List<float> lockRotationListCopy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lockListCopy = new List<GameObject>(lockList);
+        lockRotationListCopy = new List<float>(lockRotationList);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateLocks();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            ShootRay();
+        }
+    }
+
+    private void RotateLocks()
+    {
+        if(lockListCopy.Count == 0)
+        {
+            lockListCopy = new List<GameObject>(lockList);
+            lockRotationListCopy = new List<float>(lockRotationList);
+        }
+
+        int index = 0;
+        foreach (GameObject element in lockListCopy)
+        {
+            float rotationSpeed = lockRotationList[index] * Time.deltaTime;
+            element.transform.Rotate(0, rotationSpeed, 0);
+            index++; 
+        }
+    }
+
+    private int convertToHour(float angle)
+    {
+        float result = angle / 30f;
+
+        return (int)result;
+    }
+
+    private int convertToMinute(float angle)
+    {
+        float result = angle / 6f;
+
+        return (int)result;
+    }
+
+    void ShootRay()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Transform currentClockHand = hit.transform;
+
+            int index = 0;
+
+            foreach (GameObject element in lockListCopy)
+            {
+                if (currentClockHand == element.transform)
+                {
+                    index = lockListCopy.IndexOf(element);
+                }
+            }
+
+            lockListCopy.RemoveAt(index);
+            lockRotationListCopy.RemoveAt(index);
+        }
+    }
+}
