93297ebd3c02849bc4ca7f1bc3cc9a0e20c7d84d
Stefano <c.stefano6@gmail.com>
2019-05-15 09:14:01 +0200

    Added new shader, and login screens.

---
+++ DimensionCode/Assets/Scripts/Firebase/DatabaseManager.cs
@@ -0,0 +1,58 @@
+﻿using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DatabaseManager : MonoBehaviour
+{
+    public TMP_Text text;
+    public TMP_InputField mainInputField;
+    DatabaseReference reference;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set up the Editor before calling into the realtime database.
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://dimension-code.firebaseio.com/");
+
+        // Get the root reference location of the database.
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
+
+        readData();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void readData()
+    {
+        FirebaseDatabase.DefaultInstance
+       .GetReference("test")
+       .ValueChanged += HandleValueChanged;
+    }
+
+    void HandleValueChanged(object sender, ValueChangedEventArgs args)
+    {
+        if (args.DatabaseError != null)
+        {
+            Debug.LogError(args.DatabaseError.Message);
+            return;
+        }
+        // Do something with the data in args.Snapshot
+        Debug.Log(args.Snapshot.GetValue(true));
+        text.text = (string)args.Snapshot.GetValue(true);
+    }
+
+    public void sendData()
+    {
+        reference.Child("test").SetValueAsync(mainInputField.text);
+    }
+}
--- DimensionCode/Assets/Scripts/Firebase/LoginManager.cs
+++ DimensionCode/Assets/Scripts/Firebase/LoginManager.cs
@@ -25,44 +25,64 @@ public class LoginManager : MonoBehaviour
 
     public void DoLogin()
     {
-        Debug.Log($"Started Login Process with phonenumber '{phoneNumber.text}'");
-        Debug.Log("Started Login Process");
-        PhoneAuthProvider provider = PhoneAuthProvider.GetInstance(firebaseAuth);
-        provider.VerifyPhoneNumber(phoneNumber.text, 5000, null,
-          verificationCompleted: (credential) => {
-              Debug.Log("verificationCompleted");
+        firebaseAuth.SignInWithEmailAndPasswordAsync("c.stefano6@gmail.com", "123456").ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
 
-              // Auto-sms-retrieval or instant validation has succeeded (Android only).
-              // There is no need to input the verification code.
-              // `credential` can be used instead of calling GetCredential().
-          },
-          verificationFailed: (error) => {
-              Debug.Log("error");
-              // The verification code was not sent.
-              // `error` contains a human readable explanation of the problem.
-          },
-          codeSent: (id, token) => {
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
 
-              Debug.Log("codeSent");
-              Debug.Log("Id: " + id);
-              Debug.Log("Token: " + token);
 
-              idText.text = id;
-   
 
-              // Verification code was successfully sent via SMS.
-              // `id` contains the verification id that will need to passed in with
-              // the code from the user when calling GetCredential().
-              // `token` can be used if the user requests the code be sent again, to
-              // tie the two requests together.
-          },
-          codeAutoRetrievalTimeOut: (id) => {
-              Debug.Log("codeAutoRetrievalTimeOut");
-              Debug.Log("Id: " + id);
-              // Called when the auto-sms-retrieval has timed out, based on the given
-              // timeout parameter.
-              // `id` contains the verification id of the request that timed out.
-          });
+
+        //Debug.Log($"Started Login Process with phonenumber '{phoneNumber.text}'");
+        //Debug.Log("Started Login Process");
+        //PhoneAuthProvider provider = PhoneAuthProvider.GetInstance(firebaseAuth);
+        //provider.VerifyPhoneNumber(phoneNumber.text, 5000, null,
+        //  verificationCompleted: (credential) => {
+        //      Debug.Log("verificationCompleted");
+
+        //      // Auto-sms-retrieval or instant validation has succeeded (Android only).
+        //      // There is no need to input the verification code.
+        //      // `credential` can be used instead of calling GetCredential().
+        //  },
+        //  verificationFailed: (error) => {
+        //      Debug.Log("error");
+        //      // The verification code was not sent.
+        //      // `error` contains a human readable explanation of the problem.
+        //  },
+        //  codeSent: (id, token) => {
+
+        //      Debug.Log("codeSent");
+        //      Debug.Log("Id: " + id);
+        //      Debug.Log("Token: " + token);
+
+        //      idText.text = id;
+
+
+        //      // Verification code was successfully sent via SMS.
+        //      // `id` contains the verification id that will need to passed in with
+        //      // the code from the user when calling GetCredential().
+        //      // `token` can be used if the user requests the code be sent again, to
+        //      // tie the two requests together.
+        //  },
+        //  codeAutoRetrievalTimeOut: (id) => {
+        //      Debug.Log("codeAutoRetrievalTimeOut");
+        //      Debug.Log("Id: " + id);
+        //      // Called when the auto-sms-retrieval has timed out, based on the given
+        //      // timeout parameter.
+        //      // `id` contains the verification id of the request that timed out.
+        //  });
 
     }
 }
