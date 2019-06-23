a74d4d620fc4ae164219bf634c47238cbcdcc334
Stefano <c.stefano6@gmail.com>
2019-06-23 20:51:05 +0200

    Added first part of Clock Puzzle

--- DimensionCode/Assets/Scripts/Puzzles/LockPuzzle.cs
+++ DimensionCode/Assets/Scripts/Puzzles/LockPuzzle.cs
@@ -65,6 +65,7 @@ public class LockPuzzle : MonoBehaviour
 
         if (Physics.Raycast(ray, out hit))
         {
+            Debug.Log(hit.transform.name);
             Transform currentClockHand = hit.transform;
 
             int index = 0;
