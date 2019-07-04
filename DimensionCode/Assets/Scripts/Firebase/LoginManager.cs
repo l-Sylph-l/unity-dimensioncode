8f4054a09455b5b26b1f28f692dc59c1f1961ef2
Stefano <c.stefano6@gmail.com>
2019-07-04 16:13:29 +0200

    Reverted Pull Request

--- DimensionCode/Assets/Scripts/Firebase/LoginManager.cs
+++ DimensionCode/Assets/Scripts/Firebase/LoginManager.cs
@@ -7,9 +7,9 @@ using TMPro;
 public class LoginManager : MonoBehaviour
 {
     private FirebaseAuth firebaseAuth;
-    public TMP_InputField email;
-    public TMP_InputField password;
-    public TMP_Text message;
+    public TMP_InputField phoneNumber;
+    public TMP_Text idText;
+    public TMP_Text tokenText;
 
     // Start is called before the first frame update
     void Start()
@@ -20,16 +20,12 @@ public class LoginManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(firebaseAuth.CurrentUser != null)
-        {
-            message.text = firebaseAuth.CurrentUser.DisplayName;
-        }
-       
+        
     }
 
     public void DoLogin()
     {
-        firebaseAuth.SignInWithEmailAndPasswordAsync(email.text, password.text).ContinueWith(task => {
+        firebaseAuth.SignInWithEmailAndPasswordAsync("c.stefano6@gmail.com", "123456").ContinueWith(task => {
             if (task.IsCanceled)
             {
                 Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
@@ -44,8 +40,9 @@ public class LoginManager : MonoBehaviour
             Firebase.Auth.FirebaseUser newUser = task.Result;
             Debug.LogFormat("User signed in successfully: {0} ({1})",
                 newUser.DisplayName, newUser.UserId);
-            message.text = "User signed in successfully: " + newUser.DisplayName + newUser.UserId;
         });
+
+
 
 
         //Debug.Log($"Started Login Process with phonenumber '{phoneNumber.text}'");
