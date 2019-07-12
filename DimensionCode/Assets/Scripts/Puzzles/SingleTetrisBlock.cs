398f19c35f8b2df5de0576e2d6144be5e8945f95
Kim <kim.hunkeler@gmx.ch>
2019-07-12 12:54:06 +0200

    New tetris collider'n'stuff added

---
+++ DimensionCode/Assets/Scripts/Puzzles/SingleTetrisBlock.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleTetrisBlock : MonoBehaviour, InteractableInterface
+{
+    /**
+ * Start of Methods from the interact interface
+ * IMPORTANT: This Gameobject must have the Tag "Interactable"
+ */
+
+    public void Interact()
+    {
+        this.gameObject.transform.parent.Rotate(new Vector3(0f, 90f, 0f));
+
+
+    }
+}
