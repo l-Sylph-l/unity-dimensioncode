943124f4317428d80ce4b287234e72e3b663dec5
Nefaru90 <kim.hunkeler@gmx.ch>
2019-07-28 10:12:36 +0200

    Lvl2 und Lvl3 Lift changes nochmals neu hinzugefügt, sowie diverse scene changes (tags, collider etc.), Spieler kann nun jumpen

---
+++ DimensionCode/Assets/Scripts/Puzzles/GetSmallLiftLvl3Button.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetSmallLiftLvl3Button : MonoBehaviour, InteractableInterface
+{
+    bool buttonClicked = false;
+
+    public bool buttonClick
+    {
+        get { return buttonClicked; }
+        set { buttonClicked = value; }
+    }
+
+    public void Interact()
+    {
+        buttonClicked = true;
+    }
+}
---
+++ DimensionCode/Assets/Scripts/Puzzles/SmallLiftLvl3.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallLiftLvl3 : MonoBehaviour
+{
+    public GetSmallLiftLvl3Button liftButtonUp;
+    public GetSmallLiftLvl3Button liftButtonDown;
+    public GameObject liftPlatform;
+    public CharacterController player;
+    public GameObject liftCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ButtonTriggered();
+    }
+
+    public void ButtonTriggered()
+    {
+        Vector3 start = new Vector3(-1.16f, -7.26f, -1.33f);
+        Vector3 target = new Vector3(-1.16f, -2.76f, -1.33f);
+
+        if (liftButtonUp.buttonClick)
+        {
+            float step = 1f * Time.deltaTime;
+            liftPlatform.transform.localPosition = Vector3.MoveTowards(liftPlatform.transform.localPosition, target, step);
+
+            liftButtonDown.buttonClick = false;
+        }
+
+        if ((liftButtonDown.buttonClick) || (player.GetCurrentCollider().name != liftCollider.name))
+        {
+            float step = 1f * Time.deltaTime;
+            liftPlatform.transform.localPosition = Vector3.MoveTowards(liftPlatform.transform.localPosition, start, step);
+
+            liftButtonUp.buttonClick = false;
+        }
+    }
+}
